e3d9c3fafc39106ec1e269b2799b4f2d57abed32
awa94 <iris.ledesma.27@gmail.com>
2022-05-06 11:57:25 +0200

    add new role master

--- middleware/roleValidation.js
+++ middleware/roleValidation.js
@@ -1,9 +1,9 @@
 import { HttpError } from "../utils/httpError.js";
 
-export const roleValidation = (role) => {
+export const roleValidation = (roles) => {
   return (req, res, next) => {
     if (!req.user) throw new HttpError(401, "No user logged");
-    if (!["admin", role].includes(req.user.role)) throw new HttpError(403, "You dont have permission to access to this resource");
+    if (!["master", ...roles].includes(req.user.role)) throw new HttpError(403, "You dont have permission to access to this resource");
     next();
   };
 };
--- models/user.js
+++ models/user.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema(
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, enum: ["user", "admin"], default: "user", required: true },
+    role: { type: String, enum: ["user", "admin", "master"], default: "user", required: true },
   },
   {
     versionKey: false,
